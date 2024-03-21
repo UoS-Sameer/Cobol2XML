29870ae1359024f1c416f79a110176497c55aafc
naec4 <c.o.obasuyi@edu.salford.ac.uk>
2024-03-21 16:38:19 +0000

    Revert "Merge branch 'master' of https://github.com/UoS-Sameer/Cobol2XML"
    This reverts commit cb628c72514d0ec8b3791caa186741c12af17e01, reversing
    changes made to a86c9007499643ad468c2cedceb234e78157a430.

--- Cobol2XML/src/tests/ParserTest.java
+++ Cobol2XML/src/tests/ParserTest.java
@@ -14,85 +14,10 @@ class ParserTest {
 	void test() {
 		Tokenizer t = CobolParser.tokenizer();
 		Parser p = CobolParser.start();
-		
-		//DivisionName Parser Test #1
-		t.setString("identification division.");
+		t.setString("program-id. JB-base.");
 		Assembly in = new TokenAssembly(t);
 		Assembly out = p.bestMatch(in);
 		assertFalse(out.stackIsEmpty());
-		
-		// ProgramID Parser Test
-		t.setString("program-id.  base_jb.");
-		in = new TokenAssembly(t);
-		out = p.bestMatch(in);
-		assertFalse(out.stackIsEmpty());
-		
-		// DateWritten Parser Test
-		t.setString("date-written.  7-jul-1995 - mb.");
-		in = new TokenAssembly(t);
-		out = p.bestMatch(in);
-		assertFalse(out.stackIsEmpty());
-		
-		//DivisionName Parser Test #2
-		t.setString("data division.");
-		in = new TokenAssembly(t);
-		out = p.bestMatch(in);
-		assertFalse(out.stackIsEmpty());
-		
-		//SectionName Parser Test
-		t.setString("working-storage section.");
-		in = new TokenAssembly(t);
-		out = p.bestMatch(in);
-		assertFalse(out.stackIsEmpty());
-		
-		// DivisionName Parser Test #3
-		t.setString("procedure division.");
-		in = new TokenAssembly(t);
-		out = p.bestMatch(in);
-		assertFalse(out.stackIsEmpty());
-		
-		// constantValue Parser Test
-		t.setString("88 base_2 value 2.");
-		in = new TokenAssembly(t);
-		out = p.bestMatch(in);
-		assertFalse(out.stackIsEmpty());
-		
-		//computeLine Parser Test #1
-		t.setString("compute sum = num_1 + num_2.");
-		in = new TokenAssembly(t);
-		out = p.bestMatch(in);
-		assertFalse(out.stackIsEmpty());
-		
-		//computeLine Parser Test #2
-		t.setString("compute difference = num_1 - num_2.");
-		in = new TokenAssembly(t);
-		out = p.bestMatch(in);
-		assertFalse(out.stackIsEmpty());
-		
-		//computeLine Parser Test #3
-		t.setString("compute product = 13 * num_2.");
-		in = new TokenAssembly(t);
-		out = p.bestMatch(in);
-		assertFalse(out.stackIsEmpty());
-		
-		//computeLine Parser Test #4
-		t.setString("compute quotient = num_1 / 2.");
-		in = new TokenAssembly(t);
-		out = p.bestMatch(in);
-		assertFalse(out.stackIsEmpty());
-		
-		//display Parser Test
-		t.setString("display \"The sum of \" num_1 \" and \" num_2 \" is \" 10.");
-		in = new TokenAssembly(t);
-		out = p.bestMatch(in);
-		assertFalse(out.stackIsEmpty());
-		
-		//stopRun Parser Test
-		t.setString("stop run.");
-		in = new TokenAssembly(t);
-		out = p.bestMatch(in);
-		assertFalse(out.stackIsEmpty());
-		
 	}
 
 }
