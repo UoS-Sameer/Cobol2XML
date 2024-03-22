5be843def8f5966e1ad6bc3c062e1ae7daf5730c
naec4 <c.o.obasuyi@edu.salford.ac.uk>
2024-03-22 12:05:51 +0000

    Date and Division tests

---
+++ Cobol2XML/src/tests/DateAssemblerTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import cobol.DateAssembler;
+import parse.*;
+import parse.tokens.*;
+import cobol.Cobol;
+
+class DateAssemblerTest {
+
+    @Test
+    void test() {
+        DateAssembler assembler = new DateAssembler();
+        
+        // Initialize assembly with tokens
+        TokenAssembly assembly = new TokenAssembly(new TokenString("test")); 
+        assembly.push(new Token(31)); //Day
+        assembly.push(new Token("DEC/2023")); //Month + Year
+        
+        // Execute the assembler
+        assembler.workOn(assembly);
+        
+        Cobol c = (Cobol) assembly.getTarget();
+        System.out.println(c.getMonthDateWritten());
+        System.out.println(c.getYearDateWritten());
+        System.out.println(c.getDayDateWritten());
+        
+        assertEquals("DEC", c.getMonthDateWritten());
+        assertEquals(2023, c.getYearDateWritten());
+        assertEquals(31, c.getDayDateWritten());
+    }
+}
---
+++ Cobol2XML/src/tests/DivisionAssemblerTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import cobol.DivisionAssembler;
+import parse.*;
+import parse.tokens.*;
+import cobol.Cobol;
+class DivisionAssemblerTest {
+
+    @Test
+    void test() {
+        DivisionAssembler assembler = new DivisionAssembler();
+        TokenAssembly assembly = new TokenAssembly(new TokenString("test"));
+        
+        assembly.push(new Token("Data"));
+        
+        assembler.workOn(assembly);
+        
+        Cobol c = (Cobol) assembly.getTarget();
+        assertEquals("Data", c.getDivisionName());
+    }
+}
